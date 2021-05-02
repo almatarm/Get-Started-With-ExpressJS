bfe293d98de32725056d873b3d42fcd840727338
Mufeed H. AlMatar <almatarm@gmail.com>
2021-05-02 12:06:32 +0300

    Eslint with Typescript and Prettier

--- log.sh
+++ log.sh
@@ -1,20 +1,25 @@
-# Creating Directories 
+# 01A Creating Directories 
 mkdir dir logos
 cd logos
 mkdir server
 cd server
 
-# Initializing the project
+# 01B Initializing the project
 npm init
 mkdir src
 touch src/server.js
 npm install express
 npm run start
 
-# Enable TypeScript
+# 02 Enable TypeScript
 npm install --save-dev typescript
 sudo apt install node-typescript
 tsc --init
 mv src/server.js src/server.ts
 npm i --save-dev @types/express
 npm run start
+
+# 03 eslint with TypeScript
+eslint --init
+npm install --save-dev prettier eslint-config-prettier eslint-plugin-prettier
+npx prettier-init
